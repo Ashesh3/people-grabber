5a0db7d4f28fecbeffd42ffe5413aad89548de96
Ashesh3 <3626859+Ashesh3@users.noreply.github.com>
2021-05-20 12:50:41 +0530

    cleanup unused files

--- search_modules/ISearch.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import List
-
-
-class SerchModule(ABC):
-    @abstractmethod
-    @staticmethod
-    def search(doc_name: str, speciality: str) -> List[str]:
-        pass
