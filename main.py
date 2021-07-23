c4bea99f80f93765bfe50efb97e6cfa542dabba6
Ashesh3 <3626859+Ashesh3@users.noreply.github.com>
2021-07-23 21:21:36 +0530

    add youtube

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import asyncio
 import traceback
 from datetime import datetime
 from utils.config import config
-from search_modules import facebook, linkedin, twitter, instagram
+from search_modules import facebook, linkedin, twitter, instagram, youtube
 
 doc_data = DataReader(config["DOCUMENT"]["INPUT"], config["DOCUMENT"]["OUTPUT"])
 
@@ -22,12 +22,16 @@ async def main(thread_id: int, start: int, stop: int):
             doc_name = " ".join([str(row[x]) for x in config["INPUT_COLS"]])
             doc_speciality = row["specialty"]
             percent_done = ((row_id - start) / (stop - start)) * 100
-            print(f"[{thread_id}]==== {start} - #{row_id}/{stop} : {doc_name} [{doc_speciality}] [{percent_done:.2f}%] ====")
+            print(
+                f"[{thread_id}]==== {start} - #{row_id}/{stop} : {doc_name} [{doc_speciality}] [{percent_done:.2f}%] ===="
+            )
             start_time = datetime.now()
             enabled_search_modules: List[function] = []
-            for search_module in [linkedin, twitter, facebook, instagram]:
+            for search_module in [linkedin, twitter, facebook, instagram, youtube]:
                 if search_module.__name__.split(".")[-1].title() in config["SEARCH_MODULES"]:
-                    enabled_search_modules.append(search_module.search(thread_id, doc_name, doc_speciality))  # type:ignore
+                    enabled_search_modules.append(
+                        search_module.search(thread_id, doc_name, doc_speciality)
+                    )  # type:ignore
             search_results: List[ModuleResults] = await asyncio.gather(*enabled_search_modules)  # type:ignore
             print(search_results)
             print("[{}]Time elapsed: {}".format(thread_id, datetime.now() - start_time))
---
+++ search_modules/youtube.py
@@ -0,0 +1,74 @@
+from typing import Dict
+from utils.search import keywords_from_speciality
+from utils.types import *
+from utils.cache import cache
+from utils.config import config
+import requests
+
+google_keys = config["GOOGLE_SEARCH_KEYS"]
+google_api_index = 0
+
+
+async def search(thread_id: int, doc_name: str, speciality: str, max_terms: int = 5) -> ModuleResults:
+    doc_name = doc_name.lower()
+    print(f"[{thread_id}][Youtube] Searching")
+    search_hits: List[ModuleResult] = []
+    search_results = youtube_search(doc_name)[:max_terms]
+    print(f"[{thread_id}][Youtube] Channel Search: {len(search_results)} result(s)")
+    all_keywords: List[str] = []
+    for keyword_set in keywords_from_speciality(speciality):
+        all_keywords.extend(keyword_set["keywords"])
+    for result in search_results:
+        total_keywords = len(all_keywords)
+        result_content = result["content"].lower()
+        matched_keywords = sum([keyword.lower() in result_content for keyword in all_keywords])
+        confidence = round(((matched_keywords) / (total_keywords)) * 100, 2)
+        if confidence > 0:
+            search_hits.append({"link": result["channelID"], "confidence": confidence})
+    print(f"[{thread_id}][Facebook] Done")
+    return {
+        "source": "youtube",
+        "results": sorted(search_hits, key=lambda x: x["confidence"], reverse=True)[:max_terms],
+    }
+
+
+def youtube_search(search_term: str) -> List[Dict[str, str]]:
+    if f"youtube_search:{search_term}" in cache:
+        return cache[f"youtube_search:{search_term}"]
+
+    global google_api_index
+    err_count = 0
+    json_data = {}
+    while err_count < len(google_keys):
+        try:
+            res = requests.get(
+                "https://www.googleapis.com/youtube/v3/search",
+                params={
+                    "part": "snippet",
+                    "type": "channel",
+                    "key": google_keys[google_api_index % len(google_keys)],
+                    "q": search_term,
+                },
+            )
+            json_data = res.json()
+            if "error" in json_data:
+                raise ValueError("Error Scrapping Youtube.. ", json_data["error"]["message"])
+            if "items" not in json_data:
+                json_data["items"] = []
+
+            final_youtube_results = [
+                {
+                    "channelID": x["id"]["channelId"],
+                    "content": x["snippet"]["title"].lower() + x["snippet"]["description"].lower(),
+                }
+                for x in json_data["items"]
+            ]
+            cache[f"youtube_search:{search_term}"] = final_youtube_results
+            return final_youtube_results
+        except Exception as e:
+            google_api_index += 1
+            print(
+                f"YoutubeAPI Switching key... {google_api_index % len(google_keys)} [{e.__class__}: {e}] [{json_data}]"
+            )
+            err_count += 1
+    raise ValueError("Error in YoutubeAPI..")
